704ee8ea14b2b4b068118f001254d1b51c76c3cd
Bart Kieviet <b.d.kieviet@hhs.nl>
2017-07-14 14:06:34 +0200

    Options for online list checking

--- options.js
+++ options.js
@@ -61,11 +61,11 @@ function onDOMContentLoaded() {
 		'personalQLArtemisEnabled', 'allianceQLsOrionEnabled',
 		'personalQLOrionEnabled', 'allianceQLsPegasusEnabled',
 		'personalQLPegasusEnabled', 'overrideAmbushRounds',
-		'fitAmbushRounds', 'miniMap', 'sendmsgShowAlliance' );
+		'fitAmbushRounds', 'miniMap', 'sendmsgShowAlliance', 'onlinelistEnabled');
 
 	// 2. Free-form strings
 	setupControls ( 'input', onControlInput,
-		'personalQLArtemis', 'personalQLOrion', 'personalQLPegasus' );
+		'personalQLArtemis', 'personalQLOrion', 'personalQLPegasus', 'onlinelist' );
 
 	// 3. Numeric fields
 	setupControls ( 'input', onNumericControlInput,
@@ -137,7 +137,9 @@ function onDOMContentLoaded() {
 					controls.personalQLOrion );
 	wireQLControls( controls.personalQLPegasusEnabled,
 					controls.personalQLPegasus );
-
+	wireQLControls( controls.onlinelistEnabled,
+					controls.onlinelist );
+					
 	// Request the configuration
 	chrome.storage.local.get( Object.keys( controls ),
 							  onConfigurationReady );
