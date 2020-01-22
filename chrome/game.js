91cdb63d20a5fd1eefc775665d9fc185d6bac85c
Tsunder <Tsunder@users.noreply.github.com>
2020-01-22 01:54:17 -0500

    ignore feral serpent den uninhabited warnings for alarm
    makes the alarm ignore feral serpent den "warnings"

--- chrome/game.js
+++ chrome/game.js
@@ -23,7 +23,7 @@ var chrome, PSClock;
         'gnome-error.png':  'Warning',
         'gnome-info.png':   'Info' },
       IGNORE_WORDS = 
-      [ 'Sorry', 'Maintenance', 'once per second', 'failed', 'unsuccessful', 'that quickly', 'Auto-Pilot failure' ];
+      [ 'Sorry', 'Maintenance', 'once per second', 'failed', 'unsuccessful', 'that quickly', 'Auto-Pilot failure', 'uninhabited' ];
 
   var configured = false,
       msgframeLoaded = false,
