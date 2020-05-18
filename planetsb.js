77a5b332b4ad4d264fc5dfad6d59e342c27ce13e
Sei-lus <57790936+Sei-lus@users.noreply.github.com>
2020-05-18 03:22:06 +0000

    Delete planetsb.js

--- planetsb.js
+++
@@ -1,4 +0,0 @@
-(function() {
-	'use strict';
-	chrome.storage.local.get( [ Universe.getName( document )[0] + 'loc', Universe.getServer ( document ).substr( 0, 1 ) + 'mlist' ] , Mission.removeMission );
-})();
